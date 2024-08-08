d427361ee378ab27fe0dcfb2b548af60103dae64
unknown <shadeaki162@gmail.com>
2024-08-08 15:05:11 +0300

    Created footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
           <h1>Welcome To Proshop</h1>
         </Container>
       </main>
+      <Footer />
     </>
   );
 };
